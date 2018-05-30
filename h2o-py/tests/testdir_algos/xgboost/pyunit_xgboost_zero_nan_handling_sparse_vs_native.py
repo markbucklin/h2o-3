c4438c8b4148258dd7d6dc7fda6d1ee3445f3c26
Pavel Pscheidl <pavel@h2o.ai>
2018-05-30 22:01:48 +0200

    Sparse Zero vs NaN XGBoost numerical values handling test renamed

--- h2o-py/tests/testdir_algos/xgboost/pyunit_xgboost_zero_nan_handling_sparse_vs_native.py
+++ h2o-py/tests/testdir_algos/xgboost/pyunit_xgboost_zero_nan_handling_sparse_vs_native.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import xgboost as xgb
 import numpy as np;
-import scipy;
 
 from h2o.estimators.xgboost import *
 from tests import pyunit_utils
 from scipy.sparse import csr_matrix
 
 
-def xgboost_categorical_zero_nan_handling_numerical_test():
+def xgboost_categorical_zero_nan_handling_sparse_vs_native():
     assert H2OXGBoostEstimator.available() is True
 
     # Artificial data to be used throughout the test, very simple
@@ -28,7 +27,7 @@ def xgboost_categorical_zero_nan_handling_numerical_test():
     dtrain = xgb.DMatrix(data=training_data_csr, label=training_data_label)
     dtest = xgb.DMatrix(data=predict_test_data_csr)
     param = {'learning_rate': 1.0, 'silent': 1, 'objective': 'reg:linear', 'booster': 'gbtree',
-             'max_depth': 2, 'seed': 1, 'lambda': 0, 'max_delta_step': 0, 'alpha': 0, 'nround': 1,
+             'max_depth': 2, 'seed': 1, 'lambda': 0.0, 'max_delta_step': 0.0, 'alpha': 0.0, 'nround': 2,
              'tree_method': 'exact', 'max_bins': 256, 'grow_policy': 'depthwise',
              'subsample': 1.0, 'colsample_bylevel': 1.0, 'min_child_weight': 1.0, 'gamma': 0.0}
     bst = xgb.train(params=param, dtrain=dtrain, num_boost_round=10)
@@ -57,6 +56,6 @@ def xgboost_categorical_zero_nan_handling_numerical_test():
 
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(xgboost_categorical_zero_nan_handling_numerical_test)
+    pyunit_utils.standalone_test(xgboost_categorical_zero_nan_handling_sparse_vs_native)
 else:
-    xgboost_categorical_zero_nan_handling_numerical_test()
+    xgboost_categorical_zero_nan_handling_sparse_vs_native()
